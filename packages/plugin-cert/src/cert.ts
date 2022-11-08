f6f02de5fff29489c64a3c4beebdce9a73acd3b3
崇志 <chongzhi.ldq@alibaba-inc.com>
2022-11-08 21:29:09 +0800

    重写 mm cert 插件，使用社区 mkcert 包来生成本地自签名证书（解决 chrome 升级 107版本导致旧证书失效问题）

--- packages/plugin-cert/src/cert.ts
+++ packages/plugin-cert/src/cert.ts
@@ -4,17 +4,23 @@ import { info } from './commands/info'
 import { addDomains } from './commands/addDomains'
 
 export default async command => {
+  // 列出已配置的域名
   if (command.info) {
-    // 列出已配置的域名
     await info()
-  } else if (command.uninstall) {
-    // 删除本地生成的自签名证书
+  }
+
+  // 删除本地生成的自签名证书
+  else if (command.uninstall) {
     await uninstall()
-  } else if (command.add) {
-    // 添加域名
+  }
+
+  // 添加域名
+  else if (command.add) {
     await addDomains(command.add)
-  } else {
-    // 主程：生成自签证书并写入系统钥匙串
+  }
+
+  // 主程：生成自签证书并写入系统钥匙串
+  else {
     await install()
   }
 }
